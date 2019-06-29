3b68fe072d1241a238b565cad7df5e14c86e111c
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2019-06-28 22:37:06 -0500

    14

---
+++ Assets/Cuernito/Scripts_F/PlayerMov.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour {
+
+    private 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+}
